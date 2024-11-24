c73795ec1662b194c2d02178b3d404e3cf1f707d
Seda Aydemir <seda.aydemir7@gmail.com>
2024-11-24 15:02:46 +0300

    entitiy olusturuldu

---
+++ MyPortfolio/DAL/Entities/Message.cs
@@ -0,0 +1,13 @@
+﻿namespace MyPortfolio.DAL.Entities
+{
+    public class Message
+    {
+        public int MessageId { get; set; }
+        public String NameSurname{ get; set; }
+        public String Subject{ get; set; }
+        public String Email{ get; set; }
+        public String MessageDetail {  get; set; }
+        public DateTime SendDate { get; set; }
+        public bool IsRead { get; set; }
+    }
+}
