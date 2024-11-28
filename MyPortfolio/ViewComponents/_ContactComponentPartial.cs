26b6becb34fa53bd49308dccf0633f865eaf31f9
Seda Aydemir <seda.aydemir7@gmail.com>
2024-11-29 00:06:32 +0300

    Projenin son halını guncelleme

--- MyPortfolio/ViewComponents/_ContactComponentPartial.cs
+++ MyPortfolio/ViewComponents/_ContactComponentPartial.cs
@@ -1,11 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
 
 namespace MyPortfolio.ViewComponents
 {
     public class _ContactComponentPartial:ViewComponent
     {
+        MyPortfolioContext portfolioContext = new MyPortfolioContext();
         public IViewComponentResult Invoke()
-        { 
+        {
+            ViewBag.aboutTitle = portfolioContext.Contacts.Select(x => x.Title).FirstOrDefault();
+            ViewBag.description = portfolioContext.Contacts.Select(x => x.Description).FirstOrDefault();
+            ViewBag.phone1 = portfolioContext.Contacts.Select(x => x.Phone1).FirstOrDefault();
+            ViewBag.phone2 = portfolioContext.Contacts.Select(x => x.Phone2).FirstOrDefault();
+            ViewBag.email1 = portfolioContext.Contacts.Select(x => x.Email1).FirstOrDefault();
+            ViewBag.email2 = portfolioContext.Contacts.Select(x => x.Email2).FirstOrDefault();
+            ViewBag.adres = portfolioContext.Contacts.Select(x => x.Address).FirstOrDefault();
+
             return View(); 
         }
     }
